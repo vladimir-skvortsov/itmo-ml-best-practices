62eb0903ce31fbdc8b12cf94b842649893c875ff
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-11-24 01:08:18 +0300

    feat: init cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Course homework',
+    author='Vladimir Skvortsov',
+    license='MIT',
+)
